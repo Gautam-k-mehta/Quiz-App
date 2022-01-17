887e7edd206fa6fcdf8934ff3a689977f2394a71
Gautam-k-mehta <83702750+Gautam-k-mehta@users.noreply.github.com>
2022-01-17 06:32:24 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who was the first man to walk on the moon?",
+    answer: "Neil Armstrong",
+    options: [
+      "Pete Conrad",
+      "Neil Armstrong",
+      "Michael Collins",
+      "Alan Bean"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the founder of Meta?",
+    answer: "Steve Jobs",
+    options: [
+      "Steve Jobs",
+      "Bill Gates",
+      "Elon Musk",
+      "Mark Zuckerberg"
+    ]
+  },
+    {
+    numb: 3,
+    question: "During Which Year Did World War I Begin?",
+    answer: "1914",
+    options: [
+      "1914",
+      "1915",
+      "1916",
+      "1917"
+    ]
+  },
+    {
+    numb: 4,
+    question: "'.JPG' extension refers usually to what kind of file?",
+    answer: "Image File",
+    options: [
+      "System File",
+      "Java programming File",
+      "Video File",
+      "Image File"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
